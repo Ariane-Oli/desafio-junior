b2240e9ba80081ebc38fe5d484d857ffa42be452
Ariane <ariane.de@outlook.com>
2025-08-10 14:56:44 -0300

    feat: implementing TransactionController with create transaction endpoint

--- src/main/java/com/desafio/adapters/input/TransactionController.java
+++ src/main/java/com/desafio/adapters/input/TransactionController.java
@@ -2,6 +2,7 @@ package com.desafio.adapters.input;
 
 
 import com.desafio.domain.TransactionModel;
+import com.desafio.usecase.TransactionUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TransactionController {
 
-    private final TransactionUseCase transactionUseCase{
+    private final TransactionUseCase transactionUseCase;
 
-    @PostMapping("/transacao")
-    public ResponseEntity<TransactionModel> criarTransacao(@RequestBody TransactionDTO dto) {
-
+    public TransactionController(TransactionUseCase transactionUseCase) {
+        this.transactionUseCase = transactionUseCase;
 
     }
+    @PostMapping("/transacao")
+    public ResponseEntity<TransactionModel> criarTransacao(@RequestBody TransactionDTO dto) {
+        TransactionModel model = TransactionDTO.toModelMapper(dto);
+
+            TransactionModel criado = transactionUseCase.criarTransacao(model);
+
+            return ResponseEntity.status(201).body(criado);
     }
 }
