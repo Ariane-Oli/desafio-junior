f8983379ed909a392b19fb7826e121ac2ef3ce16
Ariane <ariane.de@outlook.com>
2025-08-10 11:08:36 -0300

    feature: Initial commit

---
+++ src/test/java/com/desafio_transacao/DesafioTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
