14862113ed7532d31e1507d5f7af7ff03187d45b
Ariane <ariane.de@outlook.com>
2025-08-17 17:02:22 -0300

    feat: make TransactionModel immutable and add validation

--- src/main/java/com/desafio/domain/TransactionModel.java
+++ src/main/java/com/desafio/domain/TransactionModel.java
@@ -7,10 +7,25 @@ public class TransactionModel {
     private final double valor;
    private final OffsetDateTime dataHora;
 
-    public TransactionModel(double valor, OffsetDateTime dataHora){
+    public TransactionModel(double valor, OffsetDateTime dataHora) {
+        if (dataHora == null) {
+            throw new IllegalArgumentException("dataHora é obrigatória");
+        }
+        OffsetDateTime now = OffsetDateTime.now();
+        if (dataHora.isAfter(now))
+            throw new IllegalArgumentException(("dataHora não pode ser no futuro"));
+
+
+        if (valor < 0) {
+            throw new IllegalArgumentException("valor não pode ser negativo");
+
+        }
+
+        this.valor = valor;
+        this.dataHora = dataHora;
+
 
     }
-
     public double getValor() {
         return valor;
     }
@@ -18,8 +33,6 @@ public class TransactionModel {
     public OffsetDateTime getDataHora() {
         return dataHora;
     }
-
-
 }
 
 
